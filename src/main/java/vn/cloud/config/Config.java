a51a7c023b72f3f998cc7a1cf99f1e055cfbe3d4
VoVanDuc20110635 <20110635@student.hcmute.edu.vn>
2022-11-26 01:49:00 +0700

    update lan 4

--- src/main/java/vn/cloud/config/Config.java
+++ src/main/java/vn/cloud/config/Config.java
@@ -1,9 +1,9 @@
 package vn.cloud.config;
 
 public class Config {
-	public static  String ipServer1 = "44.210.168.230"; // thay đổi ip bằng ip public trên ec2 chứa database
-	public static String userSql = "sa"; // thay đổi tài khoản đăng nhập sql container
-	public static String pasSql = "duc2112002"; // thay đổi mật khẩu đăng nhập sql container
+	public static  String ipServer1 = "34.239.76.72"; // thay đổi ip bằng ip public trên ec2 chứa database
+	public static String userSql = "SA"; // thay đổi tài khoản đăng nhập sql container
+	public static String pasSql = "MatKhau*123"; // thay đổi mật khẩu đăng nhập sql container
 	//public static String privatekeyPath = "C:\\Users\\tranc\\cloud\\newubuntu1.pem"; // thay đổi path của key ec2
 	public static String privatekeyPath = "/usr/local/tomcat/webapps/DoAnCloud.pem";
 	public static String databaseName = "usercloud";
